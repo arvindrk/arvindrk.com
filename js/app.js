96b0973b2969ffa40a64303d8a0c56ecd9e67aa9
Arvind Ram Singh Kishore <arvind.ramsinghkishore@vistaprint.com>
2018-11-30 16:41:22 -0500

    final update

--- js/app.js
+++ js/app.js
@@ -1,53 +1,76 @@
-$(function() {
+var app = angular.module( 'websiteApp', [] );
 
-	// Get the form.
-	var form = $('#ajax-contact');
+app.controller('emailController', ['$scope','$http',
+	function($scope,$http){
+		$scope.flag = false;
+		$scope.error = "";
+		$scope.data = {};
+		$scope.response = "";
+		$("#submitButton").prop("disabled", true);
 
-	// Get the messages div.
-	var formMessages = $('#form-messages');
+		$scope.calcAge = function(){
+			var birthday = new Date();
+			birthday.setFullYear(1994, 7, 13);
+			var ageDifMs = Date.now() - birthday.getTime();
+		    var ageDate = new Date(ageDifMs);
+		    $scope.age = Math.abs(ageDate.getUTCFullYear() - 1970);
+		}
 
-	// Set up an event listener for the contact form.
-	$(form).submit(function(e) {
-		// Stop the browser from submitting the form.
-		e.preventDefault();
+		$scope.validateForm = function() {
+			$scope.data.fname = $scope.fname;
+			$scope.data.lname = $scope.lname;
+			$scope.data.email = $scope.email;
+			$scope.data.phone = $scope.phone;
+			$scope.data.comments = $scope.comments;
+			
+			$scope.sendEmail();
+		}
 
-		// Serialize the form data.
-		var formData = $(form).serialize();
+		$scope.validForm = function(){
+			if($scope.fname == undefined || $scope.lname == undefined || $scope.email == undefined || $scope.comments == undefined)
+				return false;
+			
+			var data1 = $scope.fname.replace(/\s/g, '');
+			var data2 = $scope.lname.replace(/\s/g, '');
+			var data3 = $scope.email.replace(/\s/g, '');
+			var data4 = $scope.comments.replace(/\s/g, '');
+			
+			if(data1 == "" || data2 == "" || data3 == "" || data4 == "")
+				return false;
+			
+			return true;
+		}
 
-		// Submit the form using AJAX.
-		$.ajax({
-			type: 'POST',
-			url: $(form).attr('action'),
-			data: formData
-		})
-		.done(function(response) {
-			// Make sure that the formMessages div has the 'success' class.
-			$(formMessages).removeClass('error');
-			$(formMessages).addClass('success');
-
-			// Set the message text.
-			$(formMessages).text(response);
-
-			// Clear the form.
-			$('#fname').val('');
-			$('#lname').val('');
-			$('#phone').val('');
-			$('#email').val('');
-			$('#comments').val('');
-		})
-		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('success');
-			$(formMessages).addClass('error');
-
-			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+		$scope.validateFeild = function(val, id){
+			var data;
+			if(val == undefined){
+				$("#"+id).addClass('errorClass');
+				data = "";
 			}
-		});
+			else
+				data = val.replace(/\s/g, '');
+			if(data == "")
+				$("#"+id).addClass('errorClass');
+			else
+				$("#"+id).removeClass('errorClass');
+			if($scope.validForm())
+				$("#submitButton").prop("disabled", false);
+			else
+				$("#submitButton").prop("disabled", true);
+		}
 
-	});
-
-});
+		$scope.sendEmail = function(){
+			console.log($scope.data);
+			$http({
+				url : "send.php",
+				method : 'POST',
+				headers : {	"Content-Type": "application/json"	},
+				data : $scope.data
+			})
+			.then(function(response, status, headers, config) {
+				console.log(response);
+				$scope.response = "Email Sent Successfully";
+			});
+		}
+	}
+]);
--- send.php
+++ send.php
@@ -1,15 +1,17 @@
 <?php
 
-	$fname=$_POST['fname'];
-	$lname=$_POST['lname'];
-	$subject="Website Suggestions";
-  	$to="arvindsuna10@gmail.com";
-  	$email=$_POST['email'];
-  	$phone=$_POST['phone'];
-  	$feed=$_POST['comments'];
-  	$message="Form details below\n\nName : ".$fname." ".$lname."\nEmail : ".$email."\nPhone : ".$phone."\nMessage : ".$feed."\n";
-  	$header="From ".$email." Feedback ";
-
-  	mail($to,$subject,$message,$header);
- 	echo "Mail Sent Successfully!";
+  $postdata = file_get_contents("php://input");
+  $request = json_decode($postdata);
+  $fname = $request->fname;
+  $lname = $request->lname;
+  $subject = "Website Suggestions";
+  $to = "arvindsuna10@gmail.com";
+  $email = $request->email;
+  $phone = $request->phone;
+  $feed = $request->comments;
+  $message = "Form details below\n\nName : ".$fname." ".$lname."\nEmail : ".$email."\nPhone : ".$phone."\nMessage : ".$feed."\n";
+  $header = "From ".$email." Feedback ";
+  $mail = mail($to,$subject,$message,$header);
+  if($mail)
+    echo "Sent";
 ?>
