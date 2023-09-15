62e7b1efe4b5682d82b07f75ba76e78fb17bb278
Arvind Rk <Arvindsuna10@gmail.com>
2023-09-15 19:40:00 -0400

    New updates

--- return.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	$to=$_POST['email'];
-	$fname=$_POST['fname'];
-	$subject="Thank you for your valuable input"
-	$message="Hello ".$fname.",\nThank you very much for writing me your suggestions. It is very important for me and I will write you back as soon as possible if necessary. Feel free to continue dropping me emails if you any more suggestions.Have a good day :) \n\nThank you\n\nRegards\nArvind";
-	$header="From : Arvind Rk";
-	$ret=mail($to,$subject,$message,$header);
-?>
